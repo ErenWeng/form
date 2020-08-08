ec5a04a17d67098c604552a6b0f32003918cb475
ErenWeng <f245986@gmail.com>
2020-08-08 14:17:55 +0800

    modify some setting and create login page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-
+  <v-app>
+    <Login/>
+  </v-app>
 </template>
 
 <script>
+import Login from './components/Authentication/Login'
 
 export default {
   name: 'App',
   components: {
-
+    Login
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './plugins/vuetify.js'
+import vuetify from './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  vuetify,
 }).$mount('#app')
