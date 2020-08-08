894a679623c93736a792dcf3c0adfb79618c0dc8
ErenWeng <f245986@gmail.com>
2020-08-08 09:12:06 +0800

    add plugins

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/vuetify.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
-Vue.use(Vuetify)
